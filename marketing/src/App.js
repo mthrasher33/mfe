955a28f116f987b95cb3bf173ad509cb6a458d40
Matt Thrasher <matthew.j.thrasher@gmail.com>
2021-12-17 09:03:49 -0600

    Initial prettier implementation

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import {StylesProvider } from '@material-ui/core/styles';
+import React from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { StylesProvider } from '@material-ui/core/styles'
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from './components/Landing'
+import Pricing from './components/Pricing'
 
 export default () => {
   return (
-  <div>
-    <StylesProvider>
-      <BrowserRouter>
-        <Switch>
+    <div>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
             <Route exact path="/pricing">
-              <Pricing />  
-            </Route>            
-            <Route path="/">
-              <Landing />  
+              <Pricing />
             </Route>
-        </Switch>
-      </BrowserRouter>
-    </StylesProvider>
-  </div>
-  );
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  )
 }
