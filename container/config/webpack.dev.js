b35248e53bcda2c5000d5d538c087b55672661b7
Matt Thrasher <matthew.j.thrasher@gmail.com>
2021-12-17 10:42:29 -0600

    Yet another pre-commit hook test

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -5,25 +5,25 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8080,
-        historyApiFallback: {
-            index: '/index.html'
-        }
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: '/index.html',
     },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            },
-            shared: packageJson.dependencies,
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -1,15 +1,15 @@
 module.exports = {
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-runtime"],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
